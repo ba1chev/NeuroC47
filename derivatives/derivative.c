005ff9791749268afc0a69ccaa60a2348560b1b3
ba1chev <baychevyoan@gmail.com>
2026-07-06 15:49:09 +0300

    Updated

--- derivatives/derivative.c
+++ derivatives/derivative.c
@@ -9,11 +9,13 @@ vector_t* calculate_derivative_at(const derivative_t* derivative_obj, const vect
     if (!result) {
         errx(1, "malloc failed");
     }
+
     init_vector(result);
     result->data = (float*)malloc(X->size * sizeof(float));
     if (!result->data) {
         errx(1, "malloc failed");
     }
+
     result->size = X->size;
     result->capacity = X->size;
 
@@ -23,22 +25,22 @@ vector_t* calculate_derivative_at(const derivative_t* derivative_obj, const vect
         if (!left_part || !right_part) {
             errx(1, "malloc failed");
         }
+
         init_vector(left_part);
         init_vector(right_part);
         copy_vector(left_part, X);
         copy_vector(right_part, X);
 
-        left_part->data[i]  += derivative_obj->precision;
+        left_part->data[i] += derivative_obj->precision;
         right_part->data[i] -= derivative_obj->precision;
 
-        vector_t* left_val  = derivative_obj->function(left_part);
-        vector_t* right_val = derivative_obj->function(right_part);
+        vector_t* left_value = derivative_obj->function(left_part);
+        vector_t* right_value = derivative_obj->function(right_part);
+        result->data[i] = (left_value->data[i] - right_value->data[i]) / (2.0f * derivative_obj->precision);
 
-        result->data[i] = (left_val->data[i] - right_val->data[i]) / (2.0f * derivative_obj->precision);
-
-        free_vector(left_val);   free(left_val);
-        free_vector(right_val);  free(right_val);
-        free_vector(left_part);  free(left_part);
+        free_vector(left_value); free(left_value);
+        free_vector(right_value); free(right_value);
+        free_vector(left_part); free(left_part);
         free_vector(right_part); free(right_part);
     }
 
